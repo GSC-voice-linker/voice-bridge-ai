af3aa84ed062a484431f591bbaea5896d57e8fe4
leesanghoon <ilyjt0421sh@gmail.com>
2024-02-03 17:45:24 +0900

    오류로 롤백

--- integration/tester.py
+++ integration/tester.py
@@ -118,26 +118,42 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
     cap.release()
     cv2.destroyAllWindows()
     
+from google.cloud import aiplatform
 import vertexai
-from vertexai.preview.generative_models import GenerativeModel, Part
+from vertexai.language_models import TextGenerationModel
 
-prompt = "다음의 단어들을 순서대로 이용해서해서 자연스러운 대화형 문장 하나로 만들어줘. 이때 마지막이 물음표면 의문문으로 만들어줘. " + " ".join(Output)
-def generate():
-  model = GenerativeModel("gemini-pro-vision")
-  responses = model.generate_content(
-    prompt,
-    generation_config={
-        "max_output_tokens": 2048,
-        "temperature": 0.4,
-        "top_p": 1,
-        "top_k": 32
-    },
-    safety_settings=[],
-  stream=True,
-  )
-  
-  for response in responses:
-      print(response.text, end="")
+project_id = "striped-strata-411107"
+location = "asia-northeast3"
+temperature = 0.9
 
+def mk_sentence(
+    temperature: float,
+    project_id: str,
+    location: str,
+    words: list  # Initialize the words list
+) -> str:
+    """Ideation example with a Large Language Model"""
 
-generate()
+    vertexai.init(project=project_id, location=location)
+    # TODO developer - override these parameters as needed:
+    parameters = {
+        "temperature": temperature,  # Temperature controls the degree of randomness in token selection.
+        "max_output_tokens": 2048,  # Token limit determines the maximum amount of text output.
+        "top_p": 1,  # Tokens are selected from most probable to least until the sum of their probabilities equals the top_p value.
+        "top_k": 0,  # A top_k of 1 means the selected token is the most probable among all tokens.
+    }
+
+    model = TextGenerationModel.from_pretrained("text-bison@001")
+    # Combine the fixed prompt with the words list
+    prompt = "다음의 단어들을 순서대로 이용해서해서 자연스러운 대화형 문장 하나로 만들어줘. 이때 마지막이 물음표면 의문문으로 만들어줘. " + " ".join(words)
+    response = model.predict(
+        prompt,
+        **parameters,
+    )
+    print(f"Response from Model: {response.text}")
+
+    return response.text
+
+if __name__ == "__main__":
+    mk_sentence(temperature=temperature, project_id=project_id, location=location, words=Output)
+
